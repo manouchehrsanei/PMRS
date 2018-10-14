98d93f3a17414d35f0e6fd8d78bb7012ee02f2a9
Omar Duran <omaryesiduran@gmail.com>
2018-10-14 16:49:53 -0300

    ongoing work with new structure

--- TPMRSUndrainedParameters.cpp
+++ TPMRSUndrainedParameters.cpp
@@ -6,3 +6,60 @@
 //
 
 #include "TPMRSUndrainedParameters.h"
+
+
+TPMRSUndrainedParameters::TPMRSUndrainedParameters(){
+    
+    m_e_young_u = 0.0;
+    m_nu_u      = 0.0;
+    m_phi_0     = 0.0;
+    m_kappa_0   = 0.0;
+    
+}
+
+TPMRSUndrainedParameters::TPMRSUndrainedParameters(const TPMRSUndrainedParameters & other){
+    
+    m_e_young_u = other.m_e_young_u;
+    m_nu_u      = other.m_nu_u;
+    m_phi_0     = other.m_phi_0;
+    m_kappa_0   = other.m_kappa_0;
+}
+
+const TPMRSUndrainedParameters & TPMRSUndrainedParameters::operator=(const TPMRSUndrainedParameters & other){
+    
+    // check for self-assignment
+    if(&other == this){
+        return *this;
+    }
+    
+    m_e_young_u = other.m_e_young_u;
+    m_nu_u      = other.m_nu_u;
+    m_phi_0     = other.m_phi_0;
+    m_kappa_0   = other.m_kappa_0;
+    
+    return *this;
+}
+
+TPMRSUndrainedParameters::~TPMRSUndrainedParameters(){
+    
+}
+
+const std::string TPMRSUndrainedParameters::Name() const {
+    return "TPMRSUndrainedParameters";
+}
+
+void TPMRSUndrainedParameters::Print(std::ostream &out) const {
+    out << Name() << std::endl;
+    out << "m_e_young_u = " << m_e_young_u << std::endl;
+    out << "m_nu_u = " << m_nu_u << std::endl;
+    out << "m_phi_0 = " << m_phi_0 << std::endl;
+    out << "m_kappa_0 =" << m_kappa_0 << std::endl;
+}
+
+void TPMRSUndrainedParameters::SetUndrainedParameters(REAL e_young_u, REAL nu_u, REAL phi_0, REAL kappa_0){
+    m_e_young_u = e_young_u;
+    m_nu_u      = nu_u;
+    m_phi_0     = phi_0;
+    m_kappa_0   = kappa_0;
+}
+
--- TPMRSUndrainedParameters.h
+++ TPMRSUndrainedParameters.h
@@ -9,5 +9,49 @@
 #define TPMRSUndrainedParameters_h
 
 #include <stdio.h>
+#include <iostream>
+#include <string>
+#include "pzreal.h"
+
+class TPMRSUndrainedParameters {
+  
+private:
+    
+    /// Undrained Young modulus
+    REAL m_e_young_u;
+    
+    /// Undrained Poisson ratio
+    REAL m_nu_u;
+    
+    /// Initial constant porosity
+    REAL m_phi_0;
+    
+    /// Initial constant abolute permeability
+    REAL m_kappa_0;
+    
+public:
+    
+    /// Default constructor
+    TPMRSUndrainedParameters();
+    
+    /// Copy constructor
+    TPMRSUndrainedParameters(const TPMRSUndrainedParameters & other);
+    
+    /// Assignement constructor
+    const TPMRSUndrainedParameters & operator=(const TPMRSUndrainedParameters & other);
+    
+    /// Desconstructor
+    virtual ~TPMRSUndrainedParameters();
+    
+    /// Class name
+    const std::string Name() const;
+    
+    /// Print class attributes
+    virtual void Print(std::ostream &out = std::cout) const;
+    
+    ///  Set undrained parameters
+    void SetUndrainedParameters(REAL e_young_u, REAL nu_u, REAL phi_0, REAL kappa_0);
+
+};
 
 #endif /* TPMRSUndrainedParameters_h */
