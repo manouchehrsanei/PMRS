0bd357a762e56b1cd1061c4e845b02c1062dbc43
Omar Durán <omaryesiduran@gmail.com>
2019-02-18 22:52:09 -0300

    ....

--- TPMRSMonoPhasicAnalysis.cpp
+++ TPMRSMonoPhasicAnalysis.cpp
@@ -64,7 +64,7 @@ void TPMRSMonoPhasicAnalysis::ConfigurateAnalysis(DecomposeType decomposition, T
         case ELU:
         {
             
-#ifdef USING_MKL2
+#ifdef USING_MKL
             TPZSpStructMatrix struct_mat(Mesh());
             struct_mat.SetNumThreads(number_threads);
             this->SetStructuralMatrix(struct_mat);
@@ -78,7 +78,7 @@ void TPMRSMonoPhasicAnalysis::ConfigurateAnalysis(DecomposeType decomposition, T
             break;
         case ELDLt:
         {
-#ifdef USING_MKL2
+#ifdef USING_MKL
             TPZSymetricSpStructMatrix struct_mat(Mesh());
             struct_mat.SetNumThreads(number_threads);
             this->SetStructuralMatrix(struct_mat);
