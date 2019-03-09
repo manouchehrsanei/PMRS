b01ad3633a2119a485e02789c0a31fce219dffcc
Omar Duran <omaryesiduran@gmail.com>
2019-03-09 17:12:38 -0300

    ongoing with  RK

--- TPMRSRKSolver.cpp
+++ TPMRSRKSolver.cpp
@@ -21,10 +21,13 @@ TPMRSRKSolver::~TPMRSRKSolver(){
 
 /// Copy constructor
 TPMRSRKSolver::TPMRSRKSolver(const TPMRSRKSolver & other){
-    m_simulation_data       = other.m_simulation_data;
-    m_dimension             = other.m_dimension;
+    DebugStop();
 }
 
+/// Assignment constructor
+TPMRSRKSolver & TPMRSRKSolver::operator=(const TPMRSRKSolver & other){
+    DebugStop();
+}
 
 const std::string TPMRSRKSolver::Name() const{
     return "TPMRSRKSolver";
@@ -32,108 +35,45 @@ const std::string TPMRSRKSolver::Name() const{
 
 void TPMRSRKSolver::Print(std::ostream &out) const{
     out << Name() << std::endl;
-    out << "m_dimension = " << m_dimension << std::endl;
+    DebugStop();
 
 }
 
-
-REAL TPMRSRKSolver::DisplacementAnalytic(REAL &r, REAL &young, REAL &nu, REAL &biotcof, REAL &u_w, REAL &u_r, REAL &sig_0, REAL &p_0, REAL &p_w, REAL &r_o, REAL &r_w){
+std::vector<REAL> TPMRSRKSolver::f(std::vector<REAL> & y){
     
-    REAL dis = (biotcof*p_0*pow(r,2)*pow(r_o,2)*log(r) - biotcof*p_w*pow(r,2)*pow(r_o,2)*log(r) - biotcof*p_0*pow(r,2)*pow(r_w,2)*log(r) + biotcof*p_w*pow(r,2)*pow(r_w,2)*log(r) -
-           biotcof*p_0*pow(r,2)*pow(r_o,2)*log(r_o) + biotcof*p_w*pow(r,2)*pow(r_o,2)*log(r_o) + biotcof*p_0*pow(r_o,2)*pow(r_w,2)*log(r_o) -
-           biotcof*p_w*pow(r_o,2)*pow(r_w,2)*log(r_o) + (pow(r,2)*r_o*u_r*young*log(r_o))/(1 + nu) + (4*nu*pow(r,2)*r_o*u_r*young*log(r_o))/((1 - 2*nu)*(1 + nu)) -
-           (r_o*pow(r_w,2)*u_r*young*log(r_o))/(1 + nu) - (4*nu*r_o*pow(r_w,2)*u_r*young*log(r_o))/((1 - 2*nu)*(1 + nu)) - (pow(r,2)*r_w*u_w*young*log(r_o))/(1 + nu) -
-           (4*nu*pow(r,2)*r_w*u_w*young*log(r_o))/((1 - 2*nu)*(1 + nu)) + (pow(r_o,2)*r_w*u_w*young*log(r_o))/(1 + nu) +
-           (4*nu*pow(r_o,2)*r_w*u_w*young*log(r_o))/((1 - 2*nu)*(1 + nu)) + biotcof*p_0*pow(r,2)*pow(r_w,2)*log(r_w) - biotcof*p_w*pow(r,2)*pow(r_w,2)*log(r_w) -
-           biotcof*p_0*pow(r_o,2)*pow(r_w,2)*log(r_w) + biotcof*p_w*pow(r_o,2)*pow(r_w,2)*log(r_w) - (pow(r,2)*r_o*u_r*young*log(r_w))/(1 + nu) -
-           (4*nu*pow(r,2)*r_o*u_r*young*log(r_w))/((1 - 2*nu)*(1 + nu)) + (r_o*pow(r_w,2)*u_r*young*log(r_w))/(1 + nu) +
-           (4*nu*r_o*pow(r_w,2)*u_r*young*log(r_w))/((1 - 2*nu)*(1 + nu)) + (pow(r,2)*r_w*u_w*young*log(r_w))/(1 + nu) +
-           (4*nu*pow(r,2)*r_w*u_w*young*log(r_w))/((1 - 2*nu)*(1 + nu)) - (pow(r_o,2)*r_w*u_w*young*log(r_w))/(1 + nu) -
-           (4*nu*pow(r_o,2)*r_w*u_w*young*log(r_w))/((1 - 2*nu)*(1 + nu)))/
-    (2.*r*(pow(r_o,2) - pow(r_w,2))*(young/(2.*(1 + nu)) + (2*nu*young)/((1 - 2*nu)*(1 + nu)))*(log(r_o) - log(r_w)));
-    return dis;
+    
+    
+}
+
+std::vector<REAL> TPMRSRKSolver::RK2Approximation(std::vector<REAL> & y_n){
     
 }
 
 
-void TPMRSRKSolver::ConfigureSigma(){
-   
-    int n_regions = m_simulation_data->NumberOfRegions();
+std::vector<REAL> TPMRSRKSolver::RK4Approximation(std::vector<REAL> & y_n){
     
-    for (int iregion = 0; iregion < n_regions; iregion++)
-    {
-        std::tuple<TPMRSUndrainedParameters, TPMRSPoroMechParameters, TPMRSPhiParameters,TPMRSKappaParameters,TPMRSPlasticityParameters> chunk =    m_simulation_data->MaterialProps()[iregion];
-        
-        REAL E,nu;
+}
 
-        TPMRSPoroMechParameters poroperm_parameters(std::get<1>(chunk));
-        std::vector<REAL> poroperm_pars = poroperm_parameters.GetParameters();
-
-        
-        E = poroperm_pars[0];
-        nu = poroperm_pars[1];
-        
-        REAL alpha     =  0.75;
-        REAL u_w       = -0.01;
-        REAL u_r       = -0.001;
-        REAL sig_0     = -90.0e6;
-        REAL p_0       =  20.0e6;
-        REAL p_w       =  10.0e6;
-        REAL r_o       =  10.0;
-        REAL r_w       =  0.1;
-        REAL r;
-        REAL udsip;
-        
-        udsip  = DisplacementAnalytic(r, E, nu, alpha, u_w, u_r, sig_0, p_0, p_w, r_o, r_w);
-        
-
-//
-
-        
-//        TPZFNMatrix<9,REAL> Grad_du, Grad_du_Transpose, delta_e;
-//        
-//        Grad_du.Transpose(&Grad_du_Transpose);
-//        delta_e = Grad_du + Grad_du_Transpose;
-//        delta_e *= 0.5;
-//        
-//
-//        TPZFNMatrix<9,REAL> eps_t;
-//        
-//        eps_t = delta_e;
-        
-        
-        
-        TPZElasticResponse ER;
-        ER.SetEngineeringData(E, nu);
-        
-        TPZElasticCriterion Elastic;
-        Elastic.SetElasticResponse(ER);
-        
-        
-        TPZTensor<REAL> epsilon_t,sigma;
-        sigma.Zero();
-        epsilon_t.Zero();
-        
-        Elastic.ApplyStrainComputeSigma(epsilon_t, sigma);
-        
-        
-    }
+void TPMRSRKSolver::ExecuteRKApproximation(){
 
 }
 
-
-void ConfigureDisplcement(){
-    DebugStop();
+void TPMRSRKSolver::PrintRKApproximation(){
+    
 }
 
-void ConfigurePressure(){
-    DebugStop();
+REAL TPMRSRKSolver::Porosity(std::vector<REAL> & y){
+    
 }
 
-void ConfigureFlux(){
-    DebugStop();
+REAL TPMRSRKSolver::Permeability(REAL & phi){
+    
 }
 
-void RungeKuttaSolver(){
-    DebugStop();
+REAL TPMRSRKSolver::K(std::vector<REAL> & y){
+    
+}
+
+REAL TPMRSRKSolver::alpha(std::vector<REAL> & y){
+    
 }
--- TPMRSRKSolver.h
+++ TPMRSRKSolver.h
@@ -12,22 +12,49 @@
 #include <stdio.h>
 #include "pzerror.h"
 #include "pzfmatrix.h"
-
+#include "TPZTensor.h"
 #include "TPMRSSimulationData.h"
 #include "TPZElasticResponse.h"
 #include "TPZElasticCriterion.h"
 
 
+template <class T, class TMEM>
 class TPMRSRKSolver {
     
 private:
     
     /// Pointer of Simulation data
     TPMRSSimulationData * m_simulation_data;
-
     
     /// Material dimension
     int m_dimension;
+    
+    /// Number of state variables
+    int m_n_state;
+    
+    /// Number of spatial steps
+    int m_n_steps;
+    
+    /// Initial state for the vector of states variables
+    std::vector<REAL> y_0;
+    
+    /// Spatial step size
+    REAL m_dr;
+    
+    /// Wellbore region radius
+    REAL m_re;
+    
+    /// Wellbore radius
+    REAL m_rw;
+    
+    /// Array that storage the Runge-Kutta approximated solution
+    TPZFMatrix<REAL> m_r_y;
+    
+    /// Plastic integrator object composed by a yield function and a elastic predictor
+    T m_plastic_integrator;
+    
+    /// Fluid viscosity
+    REAL m_eta;
     
 public:
     
@@ -40,31 +67,78 @@ public:
     /// Copy constructor
     TPMRSRKSolver(const TPMRSRKSolver & other);
     
+    /// Assignment constructor
+    TPMRSRKSolver & operator=(const TPMRSRKSolver & other);
+    
     /// Class name
     const std::string Name() const;
     
     /// Print class attributes
-    virtual void Print(std::ostream &out = std::cout) const;
+    void Print(std::ostream &out = std::cout) const;
     
-    /// Compute the analytic solution of displacement over space
-    REAL DisplacementAnalytic(REAL &r, REAL &young, REAL &nu, REAL &biotcof, REAL &u_w, REAL &u_r, REAL &sig_0, REAL &p_0, REAL &p_w, REAL &r_o, REAL &r_w);
+    /// Set the plastic integrator
+    void SetPlasticIntegrator(T & plastic_integrator){
+        m_plastic_integrator = plastic_integrator;
+    }
     
+    /// Get the plastic integrator
+    T & GetPlasticIntegrator(){
+        return m_plastic_integrator;
+    }
     
-    /// Configurate sigma
-    void ConfigureSigma();
+    /// Set the fluid viscosity
+    void SetFluidViscosity(REAL eta){
+        m_eta = eta;
+    }
     
-    /// Configurate displacement
-    void ConfigureDisplcement();
+    /// Get the fluid viscosity
+    REAL GetFluidViscosity(){
+        return m_eta;
+    }
     
-    /// Configurate pore pressure
-    void ConfigurePressure();
+    /// Set the reservoir dimensions
+    void SetReservoirDimensions(REAL rw, REAL re){
+        m_rw = rw;
+        m_re = re;
+    }
     
-    /// Configurate flux
-    void ConfigureFlux();
+    /// Set the reservoir dimensions
+    void SetNumberOfSteps(int n_steps){
+        m_n_steps = ;
+    }
     
-    /// Runge Kutta solver
-    void RungeKuttaSolver();
-
+    /// Right hand side function
+    std::vector<REAL> f(std::vector<REAL> & y);
+    
+    /// Runge-Kutta method with second order accuracy
+    std::vector<REAL> RK2Approximation(std::vector<REAL> & y_n);
+    
+    /// Runge-Kutta method with fourth order accuracy
+    std::vector<REAL> RK4Approximation(std::vector<REAL> & y_n);
+    
+    /// Perform the Runge-Kutta approximation
+    void ExecuteRKApproximation();
+    
+    /// Print the Runge-Kutta approximation
+    void PrintRKApproximation();
+    
+    /// Function that computes total strain state
+    TPZTensor<REAL> Epsilon(std::vector<REAL> & y, std::vector<REAL> & y_n);
+    
+    /// Function that computes stress state
+    TPZTensor<REAL> Sigma(TPZTensor<REAL> & epsilon, TPZFMatrix<REAL> * Dep);
+    
+    /// Bulk modulus function
+    REAL K(std::vector<REAL> & y, std::vector<REAL> & y_n);
+    
+    /// Bulk modulus function
+    REAL alpha(REAL & K, REAL & K_s);
+    
+    /// Porosity function
+    REAL Porosity(std::vector<REAL> & y, std::vector<REAL> & y_n);
+    
+    /// Permeability function
+    REAL Permeability(REAL & phi);
 
 };
 
