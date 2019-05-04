46860bad4a73efb5a515457d9f0f55b3a6b88f3d
Omar Duran <omaryesiduran@gmail.com>
2019-05-04 00:27:43 -0300

    Adding execution scripts

---
+++ scripts/welbore_rates.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+time ./PMRS CGDrillingBoreHole_2D_spatial_h_0.xml
+time ./PMRS CGDrillingBoreHole_2D_spatial_h_1.xml
+time ./PMRS CGDrillingBoreHole_2D_spatial_h_2.xml
+time ./PMRS CGDrillingBoreHole_2D_spatial_h_3.xml
+time ./PMRS CGDrillingBoreHole_2D_spatial_h_4.xml
+time ./PMRS MFDrillingBoreHole_2D_spatial_h_0.xml
+time ./PMRS MFDrillingBoreHole_2D_spatial_h_1.xml
+time ./PMRS MFDrillingBoreHole_2D_spatial_h_2.xml
+time ./PMRS MFDrillingBoreHole_2D_spatial_h_3.xml
+time ./PMRS MFDrillingBoreHole_2D_spatial_h_4.xml
