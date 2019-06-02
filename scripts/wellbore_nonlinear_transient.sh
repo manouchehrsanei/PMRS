14a5731f0608bbb1c9a265b75c61828709b39a00
Omar Duran <omaryesiduran@gmail.com>
2019-06-02 15:41:15 -0300

    Update wellbore_nonlinear_transient.sh

--- scripts/wellbore_nonlinear_transient.sh
+++ scripts/wellbore_nonlinear_transient.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 time ./PMRS NDrilling_SFI_2D_MF_A_20.xml
 time ./PMRS NDrilling_SDM_1_2D_MF_A_20.xml
-time ./PMRS NDrilling_FDM_1_2D_MF_A_20.xml
 time ./PMRS NDrilling_SDM_2_2D_MF_A_20.xml
-time ./PMRS NDrilling_FDM_2_2D_MF_A_20.xml
 time ./PMRS NDrilling_SDM_3_2D_MF_A_20.xml
+time ./PMRS NDrilling_FDM_1_2D_MF_A_20.xml
+time ./PMRS NDrilling_FDM_2_2D_MF_A_20.xml
 time ./PMRS NDrilling_FDM_3_2D_MF_A_20.xml
