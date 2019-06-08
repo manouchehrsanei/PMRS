ff46362b660d73567cd5f071d543e9595256f6b5
Omar Duran <omaryesiduran@gmail.com>
2019-06-08 17:31:44 -0300

    Adding final results for 3D nonlinear cases.

---
+++ scripts/wellbore_nonlinear_transient_3d.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+time ./PMRS NDrilling_SFI_3D_Vertical_Stress.xml
+time ./PMRS NDrilling_SDM_1_3D_Vertical_Stress.xml
+time ./PMRS NDrilling_SDM_2_3D_Vertical_Stress.xml
+time ./PMRS NDrilling_SDM_3_3D_Vertical_Stress.xml
+time ./PMRS NDrilling_SDM_3_3D_MF_A_20.xml
